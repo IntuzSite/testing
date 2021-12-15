3575801cdd2546f9144227e08ddd55a0b44298f9
Rinku Samanta <rinku.s@virtualveb.com>
2021-12-15 19:51:10 +0530

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
-
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+  <Head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-14NXRL0FB8"></script>
+<script dangerouslySetInnerHTML={{ __html:`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-14NXRL0FB8');
+` }}>
+  
+</script>
+  </Head>
+  <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
