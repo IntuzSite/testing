58614bbf5dfb8844878c6692590211b8bea95229
Rinku Samanta <rinku.s@virtualveb.com>
2021-12-15 20:11:41 +0530

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import Head from 'next/head';
+import ReactGA from "react-ga4";
+import { useEffect } from 'react';
+import { useRouter } from 'next/dist/client/router';
+
+
 function MyApp({ Component, pageProps }) {
+  const router =  useRouter()
+
+
+  useEffect(() => {
+    ReactGA.initialize("G-14NXRL0FB8");
+  }, [])
+
+  useEffect(() => {
+    ReactGA.send("pageview");
+  }, [router.asPath])
+  
+
   return <>
-  <Head>
+  {/* <Head>
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-14NXRL0FB8"></script>
 <script dangerouslySetInnerHTML={{ __html:`
   window.dataLayer = window.dataLayer || [];
@@ -13,7 +30,7 @@ function MyApp({ Component, pageProps }) {
 ` }}>
   
 </script>
-  </Head>
+  </Head> */}
   <Component {...pageProps} />
   </>
 }
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Head from 'next/head';
+const about = () => {
+    return (
+        <div>
+            <Head>
+                <title>About us</title>
+            </Head>
+            About us
+        </div>
+    )
+}
+
+export default about
